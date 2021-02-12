a40a048d1a6cf17df29c12f86c3bc03f870dd28f
rulemanu721 <65323483+rulemanu721@users.noreply.github.com>
2021-02-11 22:06:10 -0600

    Add files via upload

---
+++ practicas/python/listas.py
@@ -0,0 +1,15 @@
+students =["Jafet", "Asafat", "Carlos"]
+print(students)
+print(students[0])
+
+
+students.append("Pablo")
+students.append("Luis")
+students.append("Luis")
+
+print(students)
+
+students.pop(3)
+
+print(students)
+
---
+++ practicas/python/variables.py
@@ -0,0 +1,22 @@
+# name="Manuel Sanchez"
+# year=2021
+# print(name)
+# print (year)
+
+#One line comment
+
+# firstname ="Luis"
+
+# first_name, Last_name, role= "Manuel", "Sanchez", "God"
+
+# print(first_name)
+# print(Last_name)
+# print(role)
+
+# first_name=Last_name=full_name="Manuel Sanchez"
+
+
+school="univa"
+
+print("I study at "+ school)
+
